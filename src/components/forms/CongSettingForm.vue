22bab0ca31d6500851a6d4197d9f849bfd1db5ce
Jezreel Tan <jezdtanslc90@gmail.com>
2024-06-16 09:35:20 +0800

    messenger contact

--- src/components/forms/CongSettingForm.vue
+++ src/components/forms/CongSettingForm.vue
@@ -9,6 +9,19 @@
         <div>
             <AssemblyForm />
         </div>
+        <div>
+            <div class="contact-form">
+                <div class="form-subtitle">Help & Questions</div>
+                <div>
+                    Do you need assistance or have any questions? Are there any features you would like to request?
+                </div>
+                <a href="https://m.me/312885485248553" target="_blank">
+                    <lord-icon src="https://cdn.lordicon.com/ayhtotha.json" trigger="hover" colors="primary:#ffffff">
+                    </lord-icon>
+                    Chat Developer
+                </a>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -72,6 +85,7 @@
         flex-direction: column;
         border-radius: 5px;
         box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
+        color: grey;
     }
 
 
@@ -85,5 +99,37 @@
         color: #3DA8EA;
         font-weight: 500;
 
+    }
+</style>
+
+<style scoped>
+    .contact-form
+    {
+        margin-top: 20px;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+
+    a
+    {
+        background: #1872aa;
+        color: white;
+        padding: 8px 20px 8px 18px;
+        cursor: pointer;
+        transition: ease-in .1s;
+        text-decoration: none;
+        font-size: small;
+
+        display: inline-flex;
+        align-items: center;
+        gap: 10px;
+        border-radius: 10px;
+        max-width: fit-content;
+    }
+
+    a:hover
+    {
+        background: #126599;
     }
 </style>
