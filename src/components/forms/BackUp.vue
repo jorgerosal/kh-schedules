43b1a245298495d0cdaaf766874331b03f1c1e22
Jezreel Tan <jezdtanslc90@gmail.com>
2024-06-16 10:47:55 +0800

    ui

---
+++ src/components/forms/BackUp.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="section-wrapper">
+        <div class="form-subtitle">Backup & Restore</div>
+        <div class="actions">
+            <div class="action-item">
+                Create Backup
+                <span> <IconGT/> </span>
+            </div>
+            <div class="action-item">
+                Restore Backup
+                <span> <IconGT/> </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import IconGT from '../icons/IconGT.vue';
+
+    
+</script>
+
+<style scoped>
+    .section-wrapper {
+        margin: 20px 0 0px;
+    }
+
+    .actions {
+        padding: 10px 0;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+
+    .action-item {
+        padding: 10px 20px;
+        cursor: pointer;
+        display: flex;
+        justify-content: space-between;
+        border: 1px rgba(250, 250, 250, 0.582) solid;
+        border-radius: 4px;
+    }
+
+    .action-item:hover {
+        color: black;
+        border: 1px rgb(218, 218, 218) solid;
+        background: rgba(250, 250, 250, 0.301);
+    }
+
+    svg {
+        height: 20px;
+        width: 20px;
+        opacity: .3;
+    }
+</style>
--- src/components/forms/CongSettingForm.vue
+++ src/components/forms/CongSettingForm.vue
@@ -10,17 +10,10 @@
             <AssemblyForm />
         </div>
         <div>
-            <div class="contact-form">
-                <div class="form-subtitle">Help & Questions</div>
-                <div>
-                    Do you need assistance or have any questions? Are there any features you would like to request?
-                </div>
-                <a href="https://m.me/312885485248553" target="_blank">
-                    <lord-icon src="https://cdn.lordicon.com/ayhtotha.json" trigger="hover" colors="primary:#ffffff">
-                    </lord-icon>
-                    Chat Developer
-                </a>
-            </div>
+            <BackUp />
+        </div>
+        <div>
+            <DevContact />
         </div>
     </div>
 </template>
@@ -31,6 +24,8 @@
     import CongFormInputs from '../forms/CongFormInputs.vue';
     import COVisitForm from '../forms/COVisitForm.vue';
     import AssemblyForm from '../forms/AssemblyForm.vue';
+    import DevContact from '../forms/DevContact.vue';
+    import BackUp from '../forms/BackUp.vue';
 
     const emits = defineEmits(['hideMe']);
     const file = useFileStore();
@@ -99,37 +94,5 @@
         color: #3DA8EA;
         font-weight: 500;
 
-    }
-</style>
-
-<style scoped>
-    .contact-form
-    {
-        margin-top: 20px;
-        display: flex;
-        flex-direction: column;
-        gap: 10px;
-    }
-
-    a
-    {
-        background: #1872aa;
-        color: white;
-        padding: 8px 20px 8px 18px;
-        cursor: pointer;
-        transition: ease-in .1s;
-        text-decoration: none;
-        font-size: small;
-
-        display: inline-flex;
-        align-items: center;
-        gap: 10px;
-        border-radius: 10px;
-        max-width: fit-content;
-    }
-
-    a:hover
-    {
-        background: #126599;
     }
 </style>
