b32ed43d57aeba9ebd7ab47f1778da45eaaa2a45
Jezreel Tan <jezdtanslc90@gmail.com>
2024-06-20 21:01:45 +0800

    Display Placeholder on Events

--- src/components/templates/template-s140/_MainTemplate.vue
+++ src/components/templates/template-s140/_MainTemplate.vue
@@ -4,8 +4,7 @@
             <template v-for="(w, i) in weeks" :key="w.order">
                 <HeaderTitle v-if="i % 2 == 0"/>
                 <WeekItem :w="w" :i="i" v-if="!w.hasEvent" />
-                <!-- TODO: load if has event here -->
-
+                <EventDisplay v-else/>
                 <div v-if="i % 2 == 1 && weeks.length > i+1" class="page-break relative">
                     <div class="s140-break no-print"></div>
                 </div>
@@ -15,11 +14,12 @@
 </template>
 
 <script setup>
+    import { computed } from 'vue';
+    import { useFileStore } from '@/stores/files';
     import HeaderTitle from '@/components/templates/template-s140/HeaderTitle.vue';
     import WeekItem from '@/components/templates/template-s140/WeekItem.vue';
-    import { useFileStore } from '@/stores/files';
-    import { computed } from 'vue';
-
+import EventDisplay from './EventDisplay.vue';
+    
     const fileStore = useFileStore()
 
     const weeks = computed(() => {
